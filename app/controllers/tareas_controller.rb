e29a386247823b3d90d3219b3e3d24999d4ba7f6
rodolfo <rodolfo.perezg0@gmail.com>
2017-09-23 15:46:35 -0300

    contador de tareas listo

--- app/controllers/tareas_controller.rb
+++ app/controllers/tareas_controller.rb
@@ -3,5 +3,13 @@ class TareasController < ApplicationController
   def index
     #muestra todas las tareas del usuario actual conectado
     @tareas = Tarea.joins(accions: :user).where("users.id = ?", current_user.id)
+    #completar tareas
+    @hecho = Accion.joins(:tarea, :user).where("users.id = ?", current_user.id)
+
+    # listado de tareas
+      # lista total de tareas
+       @listaTotal = Accion.where('user_id = ?', current_user.id).count
+       # lista tareas completas
+       @listaCompletos = Accion.where(('user_id = current_user.id') && ('hecho = true')).count
   end
 end
