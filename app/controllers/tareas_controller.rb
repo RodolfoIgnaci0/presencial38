eb0820e2bfd1cd17a25881afa10de7566fbb1372
rodolfo <rodolfo.perezg0@gmail.com>
2017-09-24 22:27:40 -0300

    arreglo contador tareas para todas las vistas

--- app/controllers/tareas_controller.rb
+++ app/controllers/tareas_controller.rb
@@ -1,8 +1,12 @@
 class TareasController < ApplicationController
   before_action :authenticate_user!
+  before_action :listaTareas
   before_action :find_tarea, only: [:show]
 
   def show
+    @users_completed = User.joins(:tareas, :accions)
+                   .select('users.email, tareas.nombre')
+                   .distinct.where("accions.hecho = ?", true)
   end
 
   def index
@@ -13,14 +17,17 @@ class TareasController < ApplicationController
                    .select('tareas.id, tareas.nombre, tareas.descripcion, tareas.photo, accions.hecho')
                    .distinct.where("users.id = ?", current_user.id)
 
-    # lista total de tareas
-    @listaTotal = Accion.where('user_id = ?', current_user.id).count
-    # lista tareas completas
-    @listaCompletos = Accion.where(user_id: current_user.id, hecho: true).count
   end
 
   private
    def find_tarea
       @tarea = Tarea.find(params[:id])
+   end
+
+   def listaTareas
+     # lista total de tareas
+     @listaTotal = Accion.where('user_id = ?', current_user.id).count
+     # lista tareas completas
+     @listaCompletos = Accion.where(user_id: current_user.id, hecho: true).count
    end
 end
