f8792e76f76a9ac24fc23baaf59045f82af3b903
rodolfo <rodolfo.perezg0@gmail.com>
2017-09-26 21:15:57 -0300

    agrega foto a ranking

--- app/controllers/tareas_controller.rb
+++ app/controllers/tareas_controller.rb
@@ -9,7 +9,7 @@ class TareasController < ApplicationController
                            .distinct.where("accions.hecho = true and tareas.id = ?", @tarea.id)
 
     @ranking = User.joins(:tareas, :accions)
-                   .select('users.email, tareas.nombre, accions.created_at')
+                   .select('users.avatar, users.email, tareas.nombre, accions.created_at')
                    .distinct.where("accions.hecho = true and tareas.id = ?", @tarea.id)
                    .order('created_at ASC').limit(5)
   end
@@ -20,6 +20,13 @@ class TareasController < ApplicationController
     @intento = User.joins(:tareas, :accions)
                    .select('tareas.id, tareas.nombre, tareas.descripcion, tareas.photo, accions.hecho')
                    .distinct.where("users.id = ?", current_user.id)
+  end
+
+  def update
+    @task = Tarea.find(params[:id])
+    @task_completed = Accion.joins(:tareas, :users).where('current_user = ? and tareas.id = ?', @user.id, @tarea.id).update(hecho: true)
+    @task_incompleted = Accion.joins(:tareas, :users).where('current_user = ? and tareas.id = ?', @user.id, @tarea.id).update(hecho: false)
+    # Accion.where('user_id = 6 and tarea_id = 32').update(hecho:true) -> funcional en rails console
   end
 
   private
